8e647c6036ec8a8e9e14259ce207e5f7c4756c01
Shugheku <pasha_buglak@mail.ru>
2024-06-27 20:00:59 +0300

    Update maintwo.py

--- maintwo.py
+++ maintwo.py
@@ -26,3 +26,17 @@ fake_traders = [
 @app.get("/trades")
 def get_trades(limit: int = 1, offset: int = 0):
     return fake_traders[offset:][:limit]
+
+
+fake_users2 = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+@app.post("/users/{user_id}")
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}
